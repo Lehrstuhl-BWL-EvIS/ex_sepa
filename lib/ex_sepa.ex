c10049fd87f2c5984f49e5843514fa29f580d657
krewast <bastian.kres@krewast.de>
2024-03-22 10:38:00 +0100

    Added files generated with "mix new ex_sepa"

---
+++ lib/ex_sepa.ex
@@ -0,0 +1,18 @@
+defmodule ExSepa do
+  @moduledoc """
+  Documentation for `ExSepa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSepa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_sepa_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSepaTest do
+  use ExUnit.Case
+  doctest ExSepa
+
+  test "greets the world" do
+    assert ExSepa.hello() == :world
+  end
+end
