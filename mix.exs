6f84808fd5b9ab255074957c400cbc32085c8ee4
Stefan Göttsching <stefan.goettsching@studium.fernuni-hagen.de>
2024-06-18 11:13:15 +0200

    The project has the recommended dependencies (Credo, ExDoc, Faker). A first commit to test the signature.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule ExSepa.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "ExSepa",
+      source_url: "https://github.com/Lehrstuhl-BWL-EvIS/ex_sepa",
+      homepage_url: "https://github.com/Lehrstuhl-BWL-EvIS/ex_sepa",
+      docs: [
+        # The main page in the docs
+        # main: "ExSepa",
+        # logo: "path/to/logo.png",
+        extras: ["README.md", "LICENSE"]
+      ]
     ]
   end
 
@@ -21,8 +32,20 @@ defmodule ExSepa.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Linter for better code consistency
+      # https://hexdocs.pm/credo/overview.html
+      # https://github.com/rrrene/credo
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+
+      # Generates the documentation for the entire project
+      # https://hexdocs.pm/ex_doc/readme.html
+      # https://github.com/elixir-lang/ex_doc
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+
+      # Generates fake data (primarily for the seed)
+      # https://hexdocs.pm/faker/readme.html
+      # https://github.com/elixirs/faker
+      {:faker, "~> 0.18", only: [:dev, :test]}
     ]
   end
 end
--- test/ex_sepa_test.exs
+++ test/ex_sepa_test.exs
@@ -1,5 +1,5 @@
 defmodule ExSepaTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest ExSepa
 
   test "greets the world" do
