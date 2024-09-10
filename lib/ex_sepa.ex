0355926ace2f30bc497ec741d45afd16adb7db8c
Stefan Göttsching <stefan.goettsching@studium.fernuni-hagen.de>
2024-09-10 12:35:18 +0200

    Added examples for the screenshots in the final thesis.

--- lib/ex_sepa.ex
+++ lib/ex_sepa.ex
@@ -1,9 +1,10 @@
 defmodule ExSepa do
-  @moduledoc """
-  Documentation for `ExSepa`.
+  @moduledoc false
+  # """
+  # Documentation for `ExSepa`.
 
-  `ExSepa.DirectDebit` enables the creation of SEPA core direct debits.
-  """
+  # `ExSepa.DirectDebit` enables the creation of SEPA core direct debits.
+  # """
 
   @doc false
   def get_eea_iban_country_codes(),
@@ -47,13 +48,15 @@ defmodule ExSepa do
       "CH",
       "GB",
       "MC",
-      "SM"
-      # ,    "VA" Vatican City State not in Faker iban list
+      "SM",
+      # Vatican City State not in Faker iban list
+      "VA"
     ]
 
-  @doc """
-  List with IBAN country codes used in IBANs according to ISO 3166
-  """
+  @doc false
+  # """
+  # List with IBAN country codes used in IBANs according to ISO 3166
+  # """
   def get_iban_country_codes(),
     do: Enum.concat(get_eea_iban_country_codes(), get_non_eea_iban_country_codes())
 
@@ -110,100 +113,110 @@ defmodule ExSepa do
       "JE",
       "MC",
       "PM",
-      "SM"
-      # ,    "VA" Vatican City State not in Faker iban list
+      "SM",
+      # Vatican City State not in Faker iban list
+      "VA"
     ]
 
-  @doc """
-  List with BIC country codes used in BICs according to ISO 3166
-  """
+  @doc false
+  # """
+  # List with BIC country codes used in BICs according to ISO 3166
+  # """
   def get_bic_country_codes(),
     do: Enum.concat(get_eea_bic_country_codes(), get_non_eea_bic_country_codes())
 
   @doc false
-  def test do
-    # struct!(ExSepa.GroupHeader, [msgId: 1, initgPtyNm: "Some club"])
-    # %ExSepa.GroupHeader{msg_id: 100, initiating_party_name: "A second one"}
-    # ExSepa.GroupHeader.new(%{msgId: 1, initgPtyNm: "Name"})
-    # ExSepa.GroupHeader.new( 1, "Incorrect input")
+  def example_one do
+    direct_debit =
+      ExSepa.DirectDebit.new(%{msg_id: "Msg-ID-001", initiating_party_name: "Initiating Party"})
 
     direct_debit =
-      ExSepa.DirectDebit.new(%{
-        "msg_id" => "Msg-ID-000100",
-        "initiating_party_name" => "Club name"
-      })
-
-    direct_debit =
-      ExSepa.CustomerDirectDebitInitiationV08.add_payment_information(
+      ExSepa.DirectDebit.add_payment_information(
         direct_debit,
         %{
-          "payment_id" => "Payment-ID-0001",
-          "due_date" => %Date{} = ~D[2024-07-24],
-          "creditor_id" => "DE00ZZZ00099999999",
-          "creditor_name" => "Creditor Name",
-          "creditor_iban" => "DE87200500001234567890",
-          "creditor_bic" => "BANKDEFFXXX"
+          payment_id: "Payment-ID-0001",
+          due_date: Date.utc_today() |> Date.add(5),
+          creditor_id: "DE00ZZZ00099999999",
+          creditor_name: "Creditor Name",
+          creditor_iban: "DE87200500001234567890"
         }
       )
 
     direct_debit =
-      ExSepa.CustomerDirectDebitInitiationV08.add_payment_information(
-        direct_debit,
-        %{
-          "payment_id" => "Payment-ID-0002",
-          "due_date" => %Date{} = ~D[2024-07-29],
-          "creditor_id" => "DE00ZZZ00099999999",
-          "creditor_name" => "Creditor Name",
-          "creditor_iban" => "DE87200500001234567890"
-        }
-      )
-
-    direct_debit =
-      ExSepa.CustomerDirectDebitInitiationV08.add_transaction_information(
+      ExSepa.DirectDebit.add_transaction_information(
         direct_debit,
         "Payment-ID-0001",
         %{
-          "end_to_end_id" => "EndToEndId-0001",
-          "amount" => 100.01,
-          "mandate_id" => "Mandate-Id-01",
-          "mandate_signing_date" => ~D[2021-01-21],
-          "debtor_name" => "Debtor Name",
-          "debtor_iban" => "CH7280005000088877766",
-          "debtor_bic" => "BANKDEFFXXX",
-          "remittance_information" => "Remittance Information 1"
+          end_to_end_id: "EndToEndId-0001",
+          amount: 100.01,
+          mandate_id: "Mandate-Id-01",
+          mandate_signing_date: ~D[2021-01-21],
+          debtor_name: "Debtor Name",
+          debtor_iban: "DE88100900001234567892",
+          remittance_information: "Invoice Example 0001"
         }
       )
 
+    ExSepa.DirectDebit.to_xml(direct_debit)
+  end
+
+  @doc false
+  def example_two do
+    # Use the pipe operator
     direct_debit =
-      ExSepa.CustomerDirectDebitInitiationV08.add_transaction_information(
-        direct_debit,
-        "Payment-ID-0002",
-        %{
-          "end_to_end_id" => "EndToEndId-0001",
-          "amount" => 100.02,
-          "mandate_id" => "Mandate-Id-02",
-          "mandate_signing_date" => ~D[2022-02-22],
-          "debtor_name" => "Debtor Two",
-          "debtor_iban" => "CH7280005000088877766",
-          "remittance_information" => "Remittance Information 2"
-        }
-      )
+      ExSepa.DirectDebit.new(%{msg_id: "Msg-ID-002", initiating_party_name: "Initiating Party"})
 
-    xml = ExSepa.DirectDebit.to_xml(direct_debit)
+    direct_debit
+    |> ExSepa.DirectDebit.add_payment_information(%{
+      payment_id: "Payment-ID-0002",
+      due_date: Date.utc_today() |> Date.add(5),
+      creditor_id: "DE00ZZZ00099999999",
+      creditor_name: "Creditor Name",
+      creditor_iban: "DE87200500001234567890"
+    })
+    |> ExSepa.DirectDebit.add_transaction_information(
+      "Payment-ID-0002",
+      %{
+        end_to_end_id: "EndToEndId-0002",
+        amount: 202.22,
+        mandate_id: "Mandate-Id-02",
+        mandate_signing_date: ~D[2022-02-22],
+        debtor_name: "Debtor Name",
+        debtor_iban: "NL62PXVC6402395035",
+        remittance_information: "Invoice Example 0002"
+      }
+    )
+    |> ExSepa.DirectDebit.to_xml()
+  end
 
+  @doc false
+  def example_three do
+    # With debtor address
+    direct_debit =
+      ExSepa.DirectDebit.new(%{msg_id: "Msg-ID-003", initiating_party_name: "Initiating Party"})
 
-    {:ok, xsddoc} = File.read(Path.expand("./lib/ex_sepa/pain.008.001.08_GBIC_4.xsd"))
-
-    {:ok, model} = :erlsom.compile_xsd(xsddoc)
-
-    :erlsom.scan(xml, model)
-
-    case :erlsom.scan(xml, model) do
-      {:ok, _out, _rest} ->
-        {:ok, xml}
-
-      {:error, reason} ->
-        {:error, reason}
-    end
+    direct_debit
+    |> ExSepa.DirectDebit.add_payment_information(%{
+      payment_id: "Payment-ID-0003",
+      due_date: Date.utc_today() |> Date.add(5),
+      creditor_id: "DE00ZZZ00099999999",
+      creditor_name: "Creditor Name",
+      creditor_iban: "DE87200500001234567890"
+    })
+    |> ExSepa.DirectDebit.add_transaction_information(
+      "Payment-ID-0003",
+      %{
+        end_to_end_id: "EndToEndId-0003",
+        amount: 330.30,
+        mandate_id: "Mandate-Id-03",
+        mandate_signing_date: ~D[2023-03-23],
+        debtor_name: "Debtor Name",
+        debtor_iban: "AD6510434606G73BA76MI9TE",
+        debtor_bic: "CASBADADXXX",
+        debtor_address: %{town_name: "Andorra la Vella", country: "AD"},
+        remittance_information: "Invoice Example 0003"
+      }
+    )
+    |> ExSepa.DirectDebit.to_xml()
   end
 end
